1a6a3a297a748a253a83226ac7925fcb9b9f9921
diegoadp <83314316+diegoadp@users.noreply.github.com>
2022-05-24 15:20:50 -0300

    Insercao de usuario com POST

--- src/main/java/br/com/diegoadp/workshopmongo/services/UserService.java
+++ src/main/java/br/com/diegoadp/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.diegoadp.workshopmongo.domain.User;
+import br.com.diegoadp.workshopmongo.dto.UserDTO;
 import br.com.diegoadp.workshopmongo.repository.UserRepository;
 import br.com.diegoadp.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 
 }
