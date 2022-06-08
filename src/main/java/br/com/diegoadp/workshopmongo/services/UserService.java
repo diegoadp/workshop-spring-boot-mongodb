af616564a23029f721320344aec5c4d16f992083
diegoadp <83314316+diegoadp@users.noreply.github.com>
2022-06-08 20:56:14 -0300

    Atualizacao de usuario com PUT

--- src/main/java/br/com/diegoadp/workshopmongo/services/UserService.java
+++ src/main/java/br/com/diegoadp/workshopmongo/services/UserService.java
@@ -29,10 +29,21 @@ public class UserService {
 	public User insert(User obj) {
 		return repository.insert(obj);
 	}
-	
+
 	public void delete(String id) {
 		findById(id);
 		repository.deleteById(id);
+	}
+
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
 	}
 
 	public User fromDTO(UserDTO objDto) {
