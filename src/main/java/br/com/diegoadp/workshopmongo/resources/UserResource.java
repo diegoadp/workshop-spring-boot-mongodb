1e1e75c9caab68b007953df7bb5d5e76935d3eb4
diegoadp <83314316+diegoadp@users.noreply.github.com>
2022-06-22 13:20:58 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/br/com/diegoadp/workshopmongo/resources/UserResource.java
+++ src/main/java/br/com/diegoadp/workshopmongo/resources/UserResource.java
@@ -1,24 +1,17 @@
 package br.com.diegoadp.workshopmongo.resources;
 
-import java.net.URI;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
+import br.com.diegoadp.workshopmongo.domain.Post;
 import br.com.diegoadp.workshopmongo.domain.User;
 import br.com.diegoadp.workshopmongo.dto.UserDTO;
 import br.com.diegoadp.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -60,6 +53,12 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 
 }
