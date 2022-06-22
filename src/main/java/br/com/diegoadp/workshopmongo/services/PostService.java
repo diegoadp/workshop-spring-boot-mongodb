dd9f23f759e544f5e6bdf66dbc5b4a0d655cad53
diegoadp <83314316+diegoadp@users.noreply.github.com>
2022-06-22 13:33:29 -0300

    Obtendo um post por id

---
+++ src/main/java/br/com/diegoadp/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package br.com.diegoadp.workshopmongo.services;
+
+import br.com.diegoadp.workshopmongo.domain.Post;
+import br.com.diegoadp.workshopmongo.repository.PostRepository;
+import br.com.diegoadp.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repository;
+
+    public Post findById(String id){
+        Optional<Post> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado"));
+    }
+
+}
