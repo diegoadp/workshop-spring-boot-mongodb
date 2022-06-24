60679fe1f22a284ccc2d3cd606954ef3fca4cec5
diegoadp <83314316+diegoadp@users.noreply.github.com>
2022-06-24 09:19:21 -0300

    Consulta simples com @query

--- src/main/java/br/com/diegoadp/workshopmongo/repository/PostRepository.java
+++ src/main/java/br/com/diegoadp/workshopmongo/repository/PostRepository.java
@@ -2,11 +2,16 @@ package br.com.diegoadp.workshopmongo.repository;
 
 import br.com.diegoadp.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-    List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
+
+    //    List<Post> findByTitleContainingIgnoreCase(String text);
 }
